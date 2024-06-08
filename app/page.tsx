39c4d0cc4f44b27d38c2d650331b4d20954143bf
Akbota <aqbotamaratqyzy@gmail.com>
2024-06-08 18:55:15 +0500

    fix: main

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,12 @@ import PostCard from "@/components/PostCard";
 import {useState, useEffect} from 'react';
 import {Post}  from "@/types/index";
 import PostList from "@/components/PostsList";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen dark:text-white items-center justify-center">
-      Main Page
+    <main className="flex min-h-screen dark:text-white items-center justify-center flex-row gap-2 text-2xl">
+      <div>Go to</div><Link href="/posts" className="font-bold dark:text-white">posts</Link>
     </main>
   );
 }
